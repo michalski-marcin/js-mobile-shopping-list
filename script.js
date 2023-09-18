4e2d67c0bfafb807ff338dd5ca910f5823cee2ff
Marcin Michalski <mkamichalski@gmail.com>
2023-09-18 21:04:56 +0200

    final steps

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSetting = {
     databaseURL: "https://js-mobile-shopping-list-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -21,28 +21,53 @@ addBtn.addEventListener("click", addToCart)
 // FUNCTIONS
 
 function addToCart() {
+    if (inputField.value) {
     let inputValue = inputField.value
     push(shoppingListDB, inputValue)
     clearInputField()
+    }
 }
 
-onValue(shoppingListDB, function(snapshot) {
-    
-    let itemsArray = Object.values(snapshot.val())
-    clearShoppingList()
-    
+onValue(shoppingListDB, function (snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
 
-    for (let i = 0; i < itemsArray.length; i++) {
-        appendItemToShoppingList(itemsArray[i])
+        clearShoppingList()
+
+
+
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+
+
+            appendItemToShoppingList(currentItem)
+        }
+    } else {
+        listElement.innerHTML = "Nothing here..."
     }
+
+
 })
 
 function clearInputField() {
     inputField.value = ""
 }
 
-function appendItemToShoppingList(itemValue) {
-    listElement.innerHTML += `<li>${itemValue}</li>`
+function appendItemToShoppingList(item) {
+    const itemID = item[0]
+    const itemValue = item[1]
+    const newElement = document.createElement("li")
+
+    newElement.textContent = itemValue
+
+    newElement.addEventListener("dblclick", function () {
+        let locationShoppingListDB = ref(database, `shoppingList/${itemID}`)
+        remove(locationShoppingListDB)
+    })
+
+    listElement.append(newElement)
 }
 
 function clearShoppingList() {
